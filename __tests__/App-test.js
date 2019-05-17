92a7d10dcae3e8e9dd0b0abc29c0a4d9f18e178e
Paul Fay <paul.fay@gonitro.com>
2019-05-17 13:17:05 +0100

    jest semi-working

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -5,6 +5,8 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import ForgotPassword from '../FirebaseLogin/screens/ForgotPassword/index';
+import InputField from '../FirebaseLogin/components/InputField';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
@@ -12,3 +14,18 @@ import renderer from 'react-test-renderer';
 it('renders correctly', () => {
   renderer.create(<App />);
 });
+
+it('test rendering of input field ', () => {
+    const TextInputComponent = renderer.create(<InputField />).toJSON();
+    expect(TextInputComponent).toMatchSnapshot();
+});
+
+// i was guessing with this, not working
+// it('test rendering of input field ', () => {
+//     const props = {
+//             placeholder: 'test'
+//         },
+//     DateInputComponent = mount(<InputField {...props} />);
+//     expect(DateInputComponent.placeholder).toEqual('test');
+// });
+
